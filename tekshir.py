e842f61042721331ffaf11211fc22db301fbfd1c
ilyosovabdulloh8-cloud <ilyosovabdulloh8@gmail.com>
2026-02-27 06:08:47 +0500

    Create tekshir.py

---
+++ tekshir.py
@@ -0,0 +1,9 @@
+from openn import *
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from openn import *
+bot = Bot(token=token())
+def adminmi(admin):
+    return str(admin) in admins()
+def ownermi(owner):
+    return str(owner) in owners()
