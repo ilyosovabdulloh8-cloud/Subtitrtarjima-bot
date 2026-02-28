428f741567d400d3de2f44fd67abd8fabba2a114
ilyosovabdulloh8-cloud <ilyosovabdulloh8@gmail.com>
2026-02-28 07:18:26 +0500

    Update tekshir.py

--- tekshir.py
+++ tekshir.py
@@ -1,6 +1,5 @@
 from openn import *
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-bot = Bot(token=token())
 def adminmi(admin):
     return str(admin) in admins()
 def ownermi(owner):
